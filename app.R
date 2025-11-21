3d9ae1009dc2625e6a0bc82dc95529ee4893e8b9
Joey Stanley <joey_stanley@byu.edu>
2025-11-21 09:29:43 -0700

    add progress bar on data processing button

--- app.R
+++ app.R
@@ -486,9 +486,14 @@ server <- function(input, output, session) {
   
   ## Get data (and subsets) ----
   full_df <- eventReactive(input$process_uploaded_button, {
-    req(input$uploaded_data)
-    read_csv(input$uploaded_data$datapath, show_col_types = FALSE) %>%
-      process_data()
+    withProgress(message = "Processing…",
+                 detail = "This may take a few seconds.",
+                 value =- 0, 
+                 {
+                   req(input$uploaded_data)
+                   read_csv(input$uploaded_data$datapath, show_col_types = FALSE) %>%
+                     process_data()
+                 })
   })
   
   midpoints_df <- eventReactive(input$process_uploaded_button, {
